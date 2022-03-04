3fb9777206f0302deb42940823307421613c48bb
Lilian Azevedo <lilian.azevedo0905@gmail.com>
2022-03-04 16:57:11 -0300

    Requisito 2

--- src/App.js
+++ src/App.js
@@ -1,19 +1,16 @@
 import React from 'react';
 import { Route, Switch } from 'react-router-dom';
-import { BrowserRouter } from 'react-router-dom/cjs/react-router-dom.min';
 import Login from './pages/Login';
 import Wallet from './pages/Wallet';
 
 function App() {
   return (
     <div>
-      <BrowserRouter>
-        <Switch>
-          <Route exact path="/" component={ Login } />
-          <Route path="/carteira" component={ Wallet } />
-    {/*       <Route path="*" component={ NotFound } /> */}
-        </Switch>
-      </BrowserRouter>
+      <Switch>
+        <Route exact path="/" component={ Login } />
+        <Route path="/carteira" component={ Wallet } />
+  {/*       <Route path="*" component={ NotFound } /> */}
+      </Switch>
     </div>);
 }
 
