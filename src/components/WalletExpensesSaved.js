48b238ca5287e946bb72c782b7c6366f1008c26e
Lilian Azevedo <lilian.azevedo0905@gmail.com>
2022-03-05 14:08:05 -0300

    Bug na 7, consertei lint

--- src/components/WalletExpensesSaved.js
+++ src/components/WalletExpensesSaved.js
@@ -8,42 +8,45 @@ class WalletExpensesSaved extends Component {
     const { expenses } = this.props;
 
     return (
-        <table>
-          <thead>
-            <tr>
-              <th>Descrição</th>
-              <th>Tag</th>
-              <th>Método de pagamento</th>
-              <th>Valor</th>
-              <th>Moeda</th>
-              <th>Câmbio utilizado</th>
-              <th>Valor convertido</th>
-              <th>Moeda de conversão</th>
-              <th>Editar/Excluir</th>
-            </tr>
-          </thead>
-          <tbody>
-            {expenses.map(({ id, value, currency,
-                method, tag, description, exchangeRates }) => (
+      <table>
+        <thead>
+          <tr>
+            <th>Descrição</th>
+            <th>Tag</th>
+            <th>Método de pagamento</th>
+            <th>Valor</th>
+            <th>Moeda</th>
+            <th>Câmbio utilizado</th>
+            <th>Valor convertido</th>
+            <th>Moeda de conversão</th>
+            <th>Editar/Excluir</th>
+          </tr>
+        </thead>
+        <tbody>
+          {expenses
+            .map(({ id, value, currency, method, tag, description, exchangeRates }) => (
               <tr key={ id }>
                 <td>{description}</td>
                 <td>{tag}</td>
                 <td>{method}</td>
                 <td>{value}</td>
                 <td>{exchangeRates[currency].name}</td>
-                {/* <td>{Number(exchangeRates[currency].ask).toFixed(2)}</td> */}
+                <td>{Number(exchangeRates[currency].ask).toFixed(2)}</td>
                 {/* { exchangeRates[currency].ask.includes('.')
-                ? <td>{Number(exchangeRates[currency].ask).toFixed(2)}</td>
-                : <td>{exchangeRates[currency].ask}.00</td>
-                } */}
-                <td>{(Number(exchangeRates[currency].ask) * Number(value)).toFixed(2)}</td>
+                        ? <td>{Number(exchangeRates[currency].ask).toFixed(2)}</td>
+                        : <td>{exchangeRates[currency].ask}.00</td>
+                        } */}
+                <td>
+                  {(Number(exchangeRates[currency].ask)
+                    * Number(value)).toFixed(2)}
+                </td>
                 <td>Real</td>
-                <td><button>Excluir</button></td>
+                <td><button type="button">Excluir</button></td>
               </tr>
             ))}
-          </tbody>
-        </table>
-      );
+        </tbody>
+      </table>
+    );
   }
 }
 
