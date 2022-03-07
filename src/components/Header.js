d8f004798df564a45ac3cc06f162809c3fb76be4
Lilian Azevedo <lilian.azevedo0905@gmail.com>
2022-03-07 17:33:09 -0300

    tentei colocar o total no estado global, funciona mas contamina os testes, verificando

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,20 +6,17 @@ import '../styles/header.css';
 class Header extends Component {
   expensesTotal = () => {
     const { expenses } = this.props;
-    if (expenses.length) {
-      const totalSum = expenses.reduce((sum, expense) => {
-        const { currency, value, exchangeRates } = expense;
-        const valueQuotation = Number(exchangeRates[currency].ask);
-        const updatedValue = Number(value) * valueQuotation;
-        sum += updatedValue;
-        return sum;
-      }, 0);
-      return totalSum.toFixed(2);
-    }
-    return 0;
+    return expenses.reduce((sum, expense) => {
+      const { currency, value, exchangeRates } = expense;
+      const valueQuotation = Number(exchangeRates[currency].ask);
+      const updatedValue = Number(value) * valueQuotation;
+      sum += updatedValue;
+      return sum;
+    }, 0);
   }
 
   render() {
+    /* const { userInfo, totalValue } = this.props; */
     const { userInfo } = this.props;
 
     return (
@@ -29,7 +26,9 @@ class Header extends Component {
         </div>
         <div className="container-info">
           <h4 data-testid="email-field">{ userInfo }</h4>
-          <h4 data-testid="total-field">{this.expensesTotal()}</h4>
+          {/* <h4 data-testid="total-field">{ totalValue > 0
+            ? totalValue.toFixed(2) : 0}</h4> */}
+          <h4 data-testid="total-field">{ this.expensesTotal() }</h4>
           <h4 data-testid="header-currency-field">BRL</h4>
         </div>
       </header>);
@@ -39,11 +38,13 @@ class Header extends Component {
 const mapStateToProps = ({ user: { email }, wallet: { expenses } }) => ({
   userInfo: email,
   expenses,
+  // totalValue,
 });
 
 Header.propTypes = {
   userInfo: string.isRequired,
   expenses: PropTypes.arrayOf(string).isRequired,
+  // totalValue: number.isRequired,
 };
 
 export default connect(mapStateToProps)(Header);
--- src/reducers/wallet.js
+++ src/reducers/wallet.js
@@ -4,6 +4,7 @@ import { ADD_EXPENSE, REMOVE_EXPENSE, EDIT_EXPENSE,
 const INITIAL_STATE = {
   currencies: [],
   expenses: [],
+  // totalValue: 0,
   isEditingExpense: false,
   expenseForEdit: '',
   hasClickedEdit: false,
@@ -12,13 +13,19 @@ const INITIAL_STATE = {
 const wallet = (state = INITIAL_STATE, action) => {
   switch (action.type) {
   case ADD_EXPENSE:
+    /* const { payload: { value, exchangeRates }} = this.action; */
     return {
       ...state,
       expenses: [...state.expenses, action.payload],
+      /* totalValue: state.totalValue +
+        (Number(action.payload.value) * Number(action.payload.exchangeRates[action.payload.currency].ask)) */
     };
   case REMOVE_EXPENSE:
+    // const { value, exchangeRates, currency } = state.expenses.find((expense) => expense.id === action.id);
     return {
       ...state,
+      /*  totalValue: state.totalValue - (Number(value) * Number(exchangeRates[currency].ask)), */
+      // Funciona, mas não sei pq não passa em um dos testes
       expenses: state.expenses.filter((expense) => expense.id !== action.id),
     };
   case EDIT_EXPENSE:
